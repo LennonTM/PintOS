29dfc601267480e8f3697d4fb86ecf1a80ba7121
Mykyta Osovskyi <mo524@ic.ac.uk>
2025-10-10 18:08:36 +0100

    [mo524 ss5624] feat(thread): on yield, insert thread in order

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -349,7 +349,10 @@ thread_yield (void)
 
   old_level = intr_disable ();
   if (cur != idle_thread) 
-    list_push_back (&ready_list, &cur->elem);
+    list_insert_ordered(&ready_list,
+                        &cur->elem,
+                        sort_threads_by_effective_priority,
+                        NULL);
   cur->status = THREAD_READY;
   schedule ();
   intr_set_level (old_level);
