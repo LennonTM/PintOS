ce9fa801b76401fc6cddc9f29eb84fbdbf4f41b3
Lennon Thomson-Moore <ldt24@ic.ac.uk>
2025-10-10 12:13:03 +0100

    [oo723 ldt24] feat: implemented fixed point arithmetic macros

---
+++ src/threads/fixed-point.h
@@ -0,0 +1,30 @@
+#ifndef FIXED_POINT_H
+#define FIXED_POINT_H
+
+#include <inttypes.h>
+
+/* Fixed Point Conversion Constant*/
+#define F 2<<13
+
+/* Converts int32_t to fixed point form*/
+#define int_to_fixed(n) (n*F)
+
+/* Converts fixed point to int32_t by rounding towards zero*/
+#define fixed_to_int_floor(x) (x/F)
+
+/* Converts fixed point to int32_t by rounding to nearest*/
+#define fixed_to_int_nearest(x) ((x>=0) ? (x+F/2) : (x-F/2))
+
+/* Adds fixed point x to integer n, returning fixed point*/
+#define addf(x,n) (x+n*F)
+
+/* Integer n is subtracted from fixed point x, returning fixed point*/
+#define subf(x,n) (x-n*F)
+
+/* Multiplies two fixed point numbers, returns fixed point */
+#define mulf(x,y) (((int64_t) x) * y / F)
+
+/* Divides two fixed point numbers (x/y), returns fixed point*/
+#define divf(x,y) (((int64_t) x) * F / y)
+
+#endif /* threads/fixed-point.h */
