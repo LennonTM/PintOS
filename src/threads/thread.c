9646dbd2cfa5122bb8a41892e52513fb5d2d6425
Mykyta Osovskyi <mo524@ic.ac.uk>
2025-10-10 12:20:48 +0100

    [mo524 ss5624] feat(thread): insert unblocked thread into ready list by priority

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -256,7 +256,10 @@ thread_unblock (struct thread *t)
 
   old_level = intr_disable ();
   ASSERT (t->status == THREAD_BLOCKED);
-  list_push_back (&ready_list, &t->elem);
+  list_insert_ordered(&ready_list,
+                      &t->elem,
+                      sort_threads_by_effective_priority,
+                      NULL);
   t->status = THREAD_READY;
   intr_set_level (old_level);
 }
