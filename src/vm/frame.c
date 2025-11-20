9c828f08bcd219c50b44146b3373e04a5378fb7b
Scott Stringer <ss5624@ic.ac.uk>
2025-11-20 13:34:45 +0000

    feat(frame): initialised frame_bitmap for tracking allocated frames

--- src/vm/frame.c
+++ src/vm/frame.c
@@ -2,14 +2,21 @@
 #include "vm/frame.h"
 #include "threads/palloc.h"
 #include "threads/malloc.h"
+#include "bitmap.h"
 
-/* Two pools: one for kernel data, one for user pages. */
+/* frame_table_entry is an array of all frame_table entries */
 static struct frame_table_entry *frame_table = NULL;
+/* A bitmap indicates whether each frame_table has yet to be allocated */
+static struct bitmap *frame_bitmap = NULL;
 
 void
 frame_table_init (void) {
     size_t user_pages = get_user_pages();
+    /* Set up frame table */
     frame_table = calloc(sizeof(struct frame_table_entry), user_pages);
     if (frame_table == NULL) 
         PANIC("Failed to malloc frame table!");
+
+    /* Set up bitmap of user_page */
+    frame_bitmap = bitmap_create (user_pages);
 }
