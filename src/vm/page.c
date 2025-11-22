ec87f8ceff4e11d4919d480f1cc64cdb670c6e4b
Mykyta Osovskyi <mo524@ic.ac.uk>
2025-11-22 22:46:07 +0000

    style(page.[ch]): change indentation to 2 spaces

--- src/vm/page.c
+++ src/vm/page.c
@@ -5,8 +5,8 @@
 unsigned
 spt_hash (const struct hash_elem *p_, void *aux UNUSED)
 {
-    const struct spt_entry *p = hash_entry (p_, struct spt_entry, elem);
-    return hash_bytes (&p->u_addr, sizeof p->u_addr);
+  const struct spt_entry *p = hash_entry (p_, struct spt_entry, elem);
+  return hash_bytes (&p->u_addr, sizeof p->u_addr);
 }
 
 /* Returns true if spt_entry a precedes spt_entry b. */
@@ -14,7 +14,7 @@ bool
 spt_less (const struct hash_elem *a_, const struct hash_elem *b_,
 void *aux UNUSED)
 {
-    const struct spt_entry *a = hash_entry (a_, struct spt_entry, elem);
-    const struct spt_entry *b = hash_entry (b_, struct spt_entry, elem);
-return a->u_addr < b->u_addr;
+  const struct spt_entry *a = hash_entry (a_, struct spt_entry, elem);
+  const struct spt_entry *b = hash_entry (b_, struct spt_entry, elem);
+  return a->u_addr < b->u_addr;
 }
--- src/vm/page.h
+++ src/vm/page.h
@@ -9,44 +9,44 @@
 /* Every page is either in a frame, in the swap partition or in the file system. 
    It could also be an all-zero page. */
 enum page_status {
-    FRAME,
-    SWAP,
-    FILE,
-    ZERO
+  FRAME,
+  SWAP,
+  FILE,
+  ZERO
 };
 
 struct file_aux {
-    struct file* ptr;     /* Pointer to the struct file. */
-    size_t offset;        /* The number of bytes offset within the file. */
-    size_t valid_bytes;   /* Number of bytes valid in the page for files*/
-    size_t invalid_bytes; /* Number of invalid bytes in the page for files*/
+  struct file* ptr;     /* Pointer to the struct file. */
+  size_t offset;        /* The number of bytes offset within the file. */
+  size_t valid_bytes;   /* Number of bytes valid in the page for files*/
+  size_t invalid_bytes; /* Number of invalid bytes in the page for files*/
 };
 
 struct swap_aux {
-    size_t index; /* Index within the swap disk. */
+  size_t index; /* Index within the swap disk. */
 };
 
 struct frame_aux {
-    uint32_t* k_addr; /*Kernel address to the frame used. */
+  uint32_t* k_addr; /*Kernel address to the frame used. */
 };
 
 /* We use a union to reduce size of struct when using mutually exclusive
    meta data between different locations page could be stored. */
 union spt_entry_aux {
-    struct file_aux file;
-    struct swap_aux swap;
-    struct frame_aux frame;
+  struct file_aux file;
+  struct swap_aux swap;
+  struct frame_aux frame;
 };
 
 /* Entry to the Supplementary Page Table. */
 struct spt_entry {
-    uint32_t* u_addr; /* User address of spt entry*/
+  uint32_t* u_addr; /* User address of spt entry*/
 
-    enum page_status status; /* Indicates how the page should be handled */
+  enum page_status status; /* Indicates how the page should be handled */
 
-    union spt_entry_aux aux; /* Meta data for the spt entry */
+  union spt_entry_aux aux; /* Meta data for the spt entry */
 
-    struct hash_elem elem;
+  struct hash_elem elem;
 };
 
 /* Returns a hash value for spt_entry p. */
